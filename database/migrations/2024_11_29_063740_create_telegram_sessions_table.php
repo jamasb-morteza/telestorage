f3520365f7ddca57550a760b13f48b73121d6280
Morteza Jamasb <jamaseb.morteza@gmail.com>
2024-11-29 10:42:23 +0330

    clean telegram storage table

--- database/migrations/2024_11_29_063740_create_telegram_sessions_table.php
+++ database/migrations/2024_11_29_063740_create_telegram_sessions_table.php
@@ -12,12 +12,21 @@ return new class extends Migration {
     {
         Schema::create('telegram_sessions', function (Blueprint $table) {
             $table->string('session_name')->primary();
+            $table->foreignId('user_id')->nullable()->constrained(
+                table: 'users',
+                column: 'id',
+                indexName: 'telegram_sessions_user_id_foreign'
+            );
             $table->string('bot_token')->nullable();
             $table->integer('api_id')->nullable();
             $table->string('api_hash')->nullable();
+            $table->string('mobile_number')->index();
             $table->boolean('is_logged_in')->default(false);
-            $table->timestamp('expires_at')->nullable();
             $table->json('additional_data')->nullable();
+            $table->tinyInteger('last_session_status')->nullable();
+            $table->string('last_session_status_text')->nullable();
+            $table->dateTime('last_session_status_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
