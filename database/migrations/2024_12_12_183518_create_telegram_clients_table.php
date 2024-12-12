55cb72a82dcc965819fa5f1a5c41df3ce2515342
Morteza Jamasb <jamaseb.morteza@gmail.com>
2024-12-12 23:38:51 +0330

    telegram clients table fix

--- database/migrations/2024_12_12_183518_create_telegram_clients_table.php
+++ database/migrations/2024_12_12_183518_create_telegram_clients_table.php
@@ -10,6 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
+        Schema::dropIfExists('telegram_clients');
         Schema::create('telegram_clients', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users', 'id');
